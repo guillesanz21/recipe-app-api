8205a692309b8a974526f2158af2c58e3f6f50b6
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-02 21:50:01 +0100

    flake more flexible with spaces, fixed some linting errors and added a test for normalize email

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+
 # The BaseUserManager class is a helper class that Django provides that we can use to create a user or superuser
 # It provides some helper functions that make it easier to work with our custom user model
 class UserManager(BaseUserManager):
@@ -23,14 +24,17 @@ class UserManager(BaseUserManager):
     # normalize_email is a helper function that comes with the BaseUserManager
     # that will normalize the email address (i.e. make the domain part all lower case)
     user = self.model(email=self.normalize_email(email), **extra_fields)
-    user.set_password(password) # encrypts the password
-    user.save(using=self._db) # supports multiple databases
+    user.set_password(password)  # encrypts the password
+    user.save(using=self._db)  # supports multiple databases
 
     return user
 
+
 # The User model is a custom model that we are creating that will inherit from:
-#  - The AbstractBaseUser class provides the core implementation of a User model, including hashed passwords and tokenized password resets
-#  - The PermissionsMixin class is a helper class that adds the fields and methods necessary to support Django's Group and Permission model
+#  - The AbstractBaseUser class provides the core implementation of a User model,
+#    including hashed passwords and tokenized password resets
+#  - The PermissionsMixin class is a helper class that adds the fields and
+#    methods necessary to support Django's Group and Permission model
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
 
@@ -42,5 +46,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     # objects is a UserManager object that we will use to manage the User model
     objects = UserManager()
 
-    # USERNAME_FIELD is a constant that we can set on the User model to specify the field that we want to use as the unique identifier for the user
+    # USERNAME_FIELD is a constant that we can set on the User model to specify
+    # the field that we want to use as the unique identifier for the user
     USERNAME_FIELD = 'email'
