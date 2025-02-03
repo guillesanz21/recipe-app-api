fd55dfc9043fb8c399516f37e9bd76f17063bd52
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-03 15:55:55 +0100

    Recipe API - recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -59,3 +60,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     # USERNAME_FIELD is a constant that we can set on the User model to specify
     # the field that we want to use as the unique identifier for the user
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+  """Recipe object."""
+  user = models.ForeignKey(
+    # This is a reference to the user model that is active in the project.
+    # In theory, we could specify the model with a string, but it would be a magic string
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE
+  )
+  title = models.CharField(max_length=255)
+  description = models.TextField(blank=True)
+  time_minutes = models.IntegerField()
+  price = models.DecimalField(max_digits=5, decimal_places=2)
+  link = models.CharField(max_length=255, blank=True)
+
+  def __str__(self):
+    return self.title
