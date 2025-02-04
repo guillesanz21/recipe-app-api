0e0d83e980af7c6b305ec6f1619b95c5b545166c
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-04 11:30:48 +0100

    Ingredients API - Create Ingredients when update recipes

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -66,10 +66,17 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update a recipe."""
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+
+        # An empty list is not None.
         if tags is not None:
             # Clear all tags from the instance. This will be useful when an empty list is provided.
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)  # Create the tags, and add them to the instance.
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         # Everything outside of the tags is assigned to the instance.
         for attr, value in validated_data.items():
