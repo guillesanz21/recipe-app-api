ae83cec4d66aee73ad548275864efa87d90329b3
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-04 10:32:11 +0100

    Tags API - Create Tags when we update recipes

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -27,15 +27,38 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create a new recipe."""
-        tags = validated_data.pop('tags', [])  # Remove tags from validated data.
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as neeeded."""
         auth_user = self.context['request'].user  # We get the authenticated user to create the tags.
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)  # Create the tag object.
             recipe.tags.add(tag_obj)  # Add the tag to the recipe object.
+
+    def create(self, validated_data):
+        """Create a new recipe."""
+        tags = validated_data.pop('tags', [])  # Remove tags from validated data.
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)   # Create the tags, and add them to the instance.
+
         return recipe
+
+    # instance: the model instance that is being updated. validated_data: the data that is being updated.
+    def update(self, instance, validated_data):
+        """Update a recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            # Clear all tags from the instance. This will be useful when an empty list is provided.
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)  # Create the tags, and add them to the instance.
+
+        # Everything outside of the tags is assigned to the instance.
+        for attr, value in validated_data.items():
+            # setattr is a python built-in function that sets the value of an attribute of an object.
+            setattr(instance, attr, value)
+
+        # Save the instance, so that the changes are reflected in the database.
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
