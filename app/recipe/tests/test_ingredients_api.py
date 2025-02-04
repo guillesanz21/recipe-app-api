a80d9ded130c670e6c137bc7259f2d8161e41c6a
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-04 11:08:51 +0100

    Ingredients API - Delete Ingredient

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -72,7 +72,7 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data[0]['name'], ingredient.name)
         self.assertEqual(res.data[0]['id'], ingredient.id)
 
-    def test_update_ingredients(self):
+    def test_update_ingredient(self):
         """Test updating an ingredient."""
         ingredient = Ingredient.objects.create(user=self.user, name='Salt')
 
@@ -82,3 +82,14 @@ class PrivateIngredientsApiTests(TestCase):
 
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload['name'])
+
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient."""
+        ingredient = Ingredient.objects.create(user=self.user, name='Cilantro')
+
+        url = get_detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
