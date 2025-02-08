f44407235a5862b4a27f7f0a6c63cb0107f658db
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-02-08 19:37:31 +0100

    added health check api

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,8 +19,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from core import views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check/', core_views.health_check, name='health-check'),
     # SpectacularAPIView is used to generate the OpenAPI schema
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     # SpectacularSwaggerView is used to generate the Swagger UI with the OpenAPI schema
